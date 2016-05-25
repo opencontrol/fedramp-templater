17fd24430c2d9bfdac6257f4a4cd9a039a0c279c
Aidan Feldman <aidan.feldman@gsa.gov>
2016-05-25 17:30:43 -0400

    add basic argument checking

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-  "github.com/opencontrol/doc-template/docx"
+	"fmt"
+	"os"
+
+	"github.com/opencontrol/doc-template/docx"
 )
 
 func main() {
-  doc := new(docx.Docx)
-  doc.ReadFile("foo.doc")
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Usage:\n\n\tfedramp-templater <input> <output>\n")
+		os.Exit(1)
+	}
+	inputPath := os.Args[1]
+	outputPath := os.Args[2]
+	fmt.Printf("Creating template %s from %s...\n", outputPath, inputPath)
+
+	doc := new(docx.Docx)
+	doc.ReadFile("foo.doc")
 }
