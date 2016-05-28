507cce9fb948c88ad7e9844ef5bb2455245bb818
Aidan Feldman <aidan.feldman@gsa.gov>
2016-05-27 20:20:21 -0400

    create the output directory if it doesnt exist

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/opencontrol/fedramp-templater/templater"
 )
 
 func parseArgs() (inputPath, outputPath string) {
 	if len(os.Args) != 3 {
-		fmt.Fprint(os.Stderr, "Usage:\n\n\tfedramp-templater <input> <output>\n\n")
-		os.Exit(1)
+		log.Fatal("Usage:\n\n\tfedramp-templater <input> <output>\n\n")
 	}
 	inputPath = os.Args[1]
 	outputPath = os.Args[2]
@@ -27,8 +27,13 @@ func main() {
 
 	err = templater.TemplatizeWordDoc(wordDoc)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		log.Fatalln(err)
+	}
+
+	outputDir := filepath.Dir(outputPath)
+	err = os.MkdirAll(outputDir, 0755)
+	if err != nil {
+		log.Fatalln(err)
 	}
 	// TODO this should use the current content, or not be a method
 	wordDoc.WriteToFile(outputPath, wordDoc.GetContent())
