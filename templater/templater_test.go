166f51d73334815dee763421eafd2ab42082cba3
Aidan Feldman <aidan.feldman@gsa.gov>
2016-05-31 14:30:53 -0400

    add tests around GetWordDoc()

--- templater/templater_test.go
+++ templater/templater_test.go
@@ -24,4 +24,18 @@ var _ = Describe("Templater", func() {
 			Expect(table.Content()).To(ContainSubstring("getResponsibleRole"))
 		})
 	})
+
+	Describe("GetWordDoc", func() {
+		It("gets the content from the doc", func() {
+			path := filepath.Join("..", "fixtures", "FedRAMP_ac-2-1_v2.1.docx")
+			doc := GetWordDoc(path)
+
+			Expect(doc.GetContent()).To(ContainSubstring("Control Enhancement"))
+		})
+
+		It("returns an empty string when the doc isn't found", func() {
+			doc := GetWordDoc("non-existent.docx")
+			Expect(doc.GetContent()).To(Equal(""))
+		})
+	})
 })
