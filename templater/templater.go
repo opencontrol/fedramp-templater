e05329b81e3d1c7b6a8bb9344206a1321ef4dc5e
Aidan Feldman <aidan.feldman@gsa.gov>
2016-07-18 08:26:30 -0500

    remove unused functions

--- templater/templater.go
+++ templater/templater.go
@@ -1,7 +1,6 @@
 package templater
 
 import (
-	"fmt"
 
 	// using fork because of https://github.com/moovweb/gokogiri/pull/93#issuecomment-215582446
 	"github.com/jbowtie/gokogiri"
@@ -16,14 +15,14 @@ func GetWordDoc(path string) (doc *docx.Docx, err error) {
 }
 
 func ParseWordXML(content []byte) (xmlDoc *xml.XmlDocument, err error) {
-  xmlDoc, err = gokogiri.ParseXml(content)
+	xmlDoc, err = gokogiri.ParseXml(content)
 	if err != nil {
 		return
 	}
 	// http://stackoverflow.com/a/27475227/358804
 	xp := xmlDoc.DocXPathCtx()
 	xp.RegisterNamespace("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main")
-  return
+	return
 }
 
 func getXMLDoc(wordDoc *docx.Docx) (xmlDoc *xml.XmlDocument, err error) {
@@ -36,17 +35,6 @@ func getXMLDoc(wordDoc *docx.Docx) (xmlDoc *xml.XmlDocument, err error) {
 // returns the tables for the controls and the control enhancements
 func findSummaryTables(doc *xml.XmlDocument) (tables []xml.Node, err error) {
 	return doc.Search("//w:tbl[contains(., 'Control Summary') or contains(., 'Control Enhancement Summary')]")
-}
-
-func printNode(node xml.Node) {
-	fmt.Printf("%#v ", node)
-	fmt.Printf("\"%s\"\n", node.Content())
-}
-
-func printNodes(nodes []xml.Node) {
-	for _, node := range nodes {
-		printNode(node)
-	}
 }
 
 func templatizeXMLDoc(doc *xml.XmlDocument) (err error) {
