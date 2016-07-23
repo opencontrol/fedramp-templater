22797c4632fad55c97c429d9e5e662c3c98ca9e2
Aidan Feldman <aidan.feldman@gsa.gov>
2016-07-23 19:14:33 -0400

    refactor helper methods out of templater test
    Makes it easier to read.

--- templater/templater_test.go
+++ templater/templater_test.go
@@ -11,26 +11,37 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+func loadSSP(name string) *ssp.Document {
+	sspPath := filepath.Join("..", "fixtures", name)
+	doc, err := ssp.Load(sspPath)
+	Expect(err).NotTo(HaveOccurred())
+
+	return doc
+}
+
+func loadOpenControlFixture() opencontrols.Data {
+	openControlDir := filepath.Join("..", "fixtures", "opencontrols")
+	openControlDir, err := filepath.Abs(openControlDir)
+	Expect(err).NotTo(HaveOccurred())
+	openControlData, errors := opencontrols.LoadFrom(openControlDir)
+	for _, err := range errors {
+		Expect(err).NotTo(HaveOccurred())
+	}
+
+	return openControlData
+}
+
 var _ = Describe("Templater", func() {
 	Describe("TemplatizeSSP", func() {
 		It("fills in the Responsible Role fields", func() {
-			sspPath := filepath.Join("..", "fixtures", "FedRAMP_ac-2-1_v2.1.docx")
-			s, err := ssp.Load(sspPath)
-			Expect(err).NotTo(HaveOccurred())
-			defer s.Close()
+			doc := loadSSP("FedRAMP_ac-2-1_v2.1.docx")
+			defer doc.Close()
+			openControlData := loadOpenControlFixture()
 
-			openControlDir := filepath.Join("..", "fixtures", "opencontrols")
-			openControlDir, err = filepath.Abs(openControlDir)
-			Expect(err).NotTo(HaveOccurred())
-			openControlData, errors := opencontrols.LoadFrom(openControlDir)
-			for _, err := range errors {
-				Expect(err).NotTo(HaveOccurred())
-			}
-
-			err = TemplatizeSSP(s, openControlData)
+			err := TemplatizeSSP(doc, openControlData)
 
 			Expect(err).NotTo(HaveOccurred())
-			content := s.Content()
+			content := doc.Content()
 			Expect(content).To(ContainSubstring(`Responsible Role: Amazon Elastic Compute Cloud: AWS Staff`))
 		})
 	})
