67b5780d9564de61e502a6ee348f81e61d051b01
Aidan Feldman <aidan.feldman@gsa.gov>
2016-07-28 23:09:22 -0400

    add a GetNarrative() method for opencontrols.Data

--- opencontrols/data.go
+++ opencontrols/data.go
@@ -5,14 +5,16 @@ import (
 	"github.com/opencontrol/compliance-masonry/models"
 )
 
+const standardKey = "NIST-800-53"
+
 // Data contains the OpenControl justification information.
 type Data struct {
 	ocd docx.OpenControlDocx
 }
 
 // LoadFrom creates a new Data struct from the provided path to an `opencontrols/` directory.
-func LoadFrom(path string) (data Data, errors []error) {
-	openControlData, errors := models.LoadData(path, "")
+func LoadFrom(dirPath string) (data Data, errors []error) {
+	openControlData, errors := models.LoadData(dirPath, "")
 	if len(errors) > 0 {
 		return
 	}
@@ -24,5 +26,10 @@ func LoadFrom(path string) (data Data, errors []error) {
 
 // GetResponsibleRoles returns the responsible role information for each component matching the specified control.
 func (d *Data) GetResponsibleRoles(control string) string {
-	return d.ocd.FormatResponsibleRoles("NIST-800-53", control)
+	return d.ocd.FormatResponsibleRoles(standardKey, control)
+}
+
+// GetNarrative returns the justification text for the specified control. Pass an empty string for `sectionKey` if you are looking for the overall narrative.
+func (d *Data) GetNarrative(control string, sectionKey string) string {
+	return d.ocd.FormatNarrative(standardKey, control, sectionKey)
 }
