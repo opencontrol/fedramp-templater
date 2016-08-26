6f7e08f43ce237f2ffe84267b8d2f56f77a4c3d0
James C. Scott <jamesc.scott@gsa.gov>
2016-08-26 08:11:59 -0400

    Change scope of reporter test package

--- control/reporter_test.go
+++ control/reporter_test.go
@@ -1,10 +1,11 @@
-package control
+package control_test
 
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/opencontrol/fedramp-templater/reporter"
 	"bytes"
+	"github.com/opencontrol/fedramp-templater/control"
 )
 
 func createFakeStdOut() *bytes.Buffer {
@@ -15,14 +16,14 @@ var _ = Describe("Reporter", func() {
 	Describe("NewDiff", func() {
 		It("should return a new reporter. (Will fail to compile if it doesn't comply with the interface)", func() {
 			var diff reporter.Reporter
-			diff = NewDiff("control", "myfield", "sspValue", "yamlValue")
+			diff = control.NewDiff("control", "myfield", "sspValue", "yamlValue")
 			_ = diff
 		})
 	})
 	Describe("WriteTextTo", func() {
 		It("should write data in a plain text to the writer", func() {
 			var diff reporter.Reporter
-			diff = NewDiff("control", "myfield", "sspValue", "yamlValue")
+			diff = control.NewDiff("control", "myfield", "sspValue", "yamlValue")
 			fakeConsole := createFakeStdOut()
 			diff.WriteTextTo(fakeConsole)
 			Expect(fakeConsole.String()).To(Equal("Control: control. myfield in SSP: \"sspValue\". myfield in YAML: \"yamlValue\".\n"))
