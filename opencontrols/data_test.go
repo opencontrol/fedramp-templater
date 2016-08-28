8556567d8965503cadf8bbaad0e34d38a53ceaa0
Aidan Feldman <aidan.feldman@gsa.gov>
2016-08-27 21:44:35 -0400

    fix narrative tests

--- opencontrols/data_test.go
+++ opencontrols/data_test.go
@@ -1,37 +1,22 @@
 package opencontrols_test
 
 import (
-	"path/filepath"
-
-	. "github.com/opencontrol/fedramp-templater/opencontrols"
+	"github.com/opencontrol/fedramp-templater/fixtures"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-// TODO share with templater_test
-func loadOpenControlFixture() Data {
-	openControlDir := filepath.Join("..", "fixtures", "opencontrols")
-	openControlDir, err := filepath.Abs(openControlDir)
-	Expect(err).NotTo(HaveOccurred())
-	openControlData, errors := LoadFrom(openControlDir)
-	for _, err := range errors {
-		Expect(err).NotTo(HaveOccurred())
-	}
-
-	return openControlData
-}
-
 var _ = Describe("Data", func() {
 	Describe("GetNarrative", func() {
 		It("returns the relevant singular narrative", func() {
-			data := loadOpenControlFixture()
+			data := fixtures.LoadOpenControlFixture()
 			result := data.GetNarrative("AC-2 (1)", "")
 			Expect(result).To(Equal("Amazon Elastic Compute Cloud\nJustification in narrative form for AC-2 (1)\n"))
 		})
 
 		It("returns the relevant narrative section", func() {
-			data := loadOpenControlFixture()
+			data := fixtures.LoadOpenControlFixture()
 			result := data.GetNarrative("AC-2", "a")
 			Expect(result).To(Equal("Amazon Elastic Compute Cloud\nJustification in narrative form A for AC-2\n"))
 		})
