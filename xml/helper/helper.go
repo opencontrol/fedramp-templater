3965568ebb2b993b418ea0b4f49a498f0c321dc8
Aidan Feldman <aidan.feldman@gsa.gov>
2016-08-28 12:52:47 -0400

    create an XML helper package

---
+++ xml/helper/helper.go
@@ -0,0 +1,28 @@
+package helper
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/jbowtie/gokogiri/xml"
+)
+
+// SearchSubtree searches the subtree of the given root node.
+func SearchSubtree(root xml.Node, xpath string) (nodes []xml.Node, err error) {
+	// http://stackoverflow.com/a/25387687/358804
+	if !strings.HasPrefix(xpath, ".") {
+		err = errors.New("XPath must have leading period (`.`) to only search the subtree")
+		return
+	}
+
+	return root.Search(xpath)
+}
+
+// SearchOne searches the subtree of the given root node and returns the first result.
+func SearchOne(root xml.Node, xpath string) (xml.Node, error) {
+	results, err := SearchSubtree(root, xpath)
+	if err != nil {
+		return nil, err
+	}
+	return results[0], nil
+}
