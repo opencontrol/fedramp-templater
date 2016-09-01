aa0930fb472af69f4c5885af230360f84b272c1d
James C. Scott <jamesc.scott@gsa.gov>
2016-09-01 09:26:46 -0400

    Clean up control origination tests

--- control/control_origination_test.go
+++ control/control_origination_test.go
@@ -20,41 +20,6 @@ var _ = Describe("controlOrignation", func() {
 			Expect(err).ToNot(HaveOccurred())
 			// Check number of control origination.
 			Expect(len(co.origins)).To(Equal(7))
-
-			// Find the unchecked service provided corporate origination.
-			Expect(co.origins[serviceProviderCorporateOrigination].GetTextValue()).
-				To(ContainSubstring(serviceProviderCorporateOrigination))
-			Expect(co.origins[serviceProviderCorporateOrigination].IsChecked()).To(Equal(false))
-
-			// Find the unchecked service provided system specific origination.
-			Expect(co.origins[serviceProviderSystemSpecificOrigination].GetTextValue()).
-				To(ContainSubstring(serviceProviderSystemSpecificOrigination))
-			Expect(co.origins[serviceProviderSystemSpecificOrigination].IsChecked()).To(Equal(false))
-
-			// Find the unchecked service provided hybrid origination.
-			Expect(co.origins[serviceProviderHybridOrigination].GetTextValue()).
-				To(ContainSubstring(serviceProviderHybridOrigination))
-			Expect(co.origins[serviceProviderHybridOrigination].IsChecked()).To(Equal(false))
-
-			// Find the unchecked configured by customer origination.
-			Expect(co.origins[configuredByCustomerOrigination].GetTextValue()).
-				To(ContainSubstring(configuredByCustomerOrigination))
-			Expect(co.origins[configuredByCustomerOrigination].IsChecked()).To(Equal(false))
-
-			// Find the unchecked provided by customer origination.
-			Expect(co.origins[providedByCustomerOrigination].GetTextValue()).
-				To(ContainSubstring(providedByCustomerOrigination))
-			Expect(co.origins[providedByCustomerOrigination].IsChecked()).To(Equal(false))
-
-			// Find the unchecked shared origination.
-			Expect(co.origins[sharedOrigination].GetTextValue()).
-				To(ContainSubstring(sharedOrigination))
-			Expect(co.origins[sharedOrigination].IsChecked()).To(Equal(false))
-
-			// Find the unchecked inherited origination.
-			Expect(co.origins[inheritedOrigination].GetTextValue()).
-				To(ContainSubstring(inheritedOrigination))
-			Expect(co.origins[inheritedOrigination].IsChecked()).To(Equal(false))
 		})
 	})
 })
